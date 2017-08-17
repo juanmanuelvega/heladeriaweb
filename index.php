e6186b44a2f74161718ac7a3105442869175e23e
juanmanuelvega <masst666@gmail.com>
2017-08-17 14:32:08 -0300

    probamos hacer un pequeño cambio en el comentareado del index

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Importante el uso de la clase ControladorPrincipal
 require_once("app/core/ControladorPrincipal.php");
 ControladorPrincipal::encabezadoHtml();
 ControladorPrincipal::enlaces();
